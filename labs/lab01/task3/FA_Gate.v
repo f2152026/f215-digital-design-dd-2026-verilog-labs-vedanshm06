b8f8f37b51d0c51fbe73baecc3c3927555f0f6d3
Vedansh Mahajan <f20240933@pilani.bits-pilani.ac.in>
2026-08-29 16:27:55 +0000

    lab01 task3

---
+++ labs/lab01/task3/FA_Gate.v
@@ -0,0 +1,16 @@
+module FA_Gate(
+  input  a,
+  input  b,
+  input  cin,
+  output sum,
+  output cout
+);
+  wire ps, pc1, pc2;
+
+  xor #(2) (ps,  a,   b);
+  and #(2) (pc1, a,   b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or  #(2) (cout, pc1, pc2);
+
+endmodule
